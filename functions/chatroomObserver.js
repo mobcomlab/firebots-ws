c3badbfb2e3c891316de530f5bc94819c63fb541
Charles Allen <charles.allen@codeconfident.com>
2017-09-30 18:56:59 +0700

    Bots - Initialize app; Add logging to promise

--- functions/chatroomObserver.js
+++ functions/chatroomObserver.js
@@ -1,22 +1,25 @@
 const admin = require('firebase-admin'); // The Firebase Admin SDK to access the Firebase Realtime Database.
 
-// Setup database
 const database = admin.database();
 
 exports.chatroomMessageCreatedHandler = (event) => {
     const messageID = event.params.pushId;
     const message = event.data.val();
 
-    if (!message['isBot']) {
+    if(!message['isBot']) {
         let newMessage = {
             isBot: true,
-            text: 'Hello '+message['senderName']+'.',
+            text: 'Hello ' + message['senderName'] + '.',
             isFirstMessageOfDate: false,
             sendingTime: '2017-09-30T09:47:47.593Z',
             senderId: 'FireBots',
             senderName: 'FireBots'
         };
 
-        database.ref('chatroom').child('message').push().set(newMessage);
+        return database.ref('chatroom').child('message')
+            .push().set(newMessage)
+            .then(() => {
+                console.log('Welcome message sent');
+            });
     }
 };
--- functions/index.js
+++ functions/index.js
@@ -2,6 +2,7 @@
 
 const functions = require('firebase-functions');
 const admin = require('firebase-admin'); // The Firebase Admin SDK to access the Firebase Realtime Database.
+admin.initializeApp(functions.config().firebase);
 
 const chatroomObserverModule = require('./chatroomObserver');
 
