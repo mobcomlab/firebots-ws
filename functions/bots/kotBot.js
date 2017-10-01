6ef441d1eb469265890227c699d59562bb383879
Charles Allen <charles.allen@codeconfident.com>
2017-10-01 15:29:59 +0700

    KotBot - Remember historical commands and restore each time

--- functions/bots/kotBot.js
+++ functions/bots/kotBot.js
@@ -1,8 +1,12 @@
 'use strict';
 
+const admin = require('firebase-admin');
+const database = admin.database();
+
 const request = require('request');
 const chat = require('../firebots/chatInterface');
 
+const lineBreak = "\r\n";
 const botId = '2';
 
 exports.getName = function() {
@@ -27,22 +31,69 @@ exports.onUserMessage = function(chatRoomId, message) {
     if(input.startsWith("!k")) {
         const command = input.substring(2);
 
-        request.post({url: url, headers: headers, body: command}, function (error, response, body) {
-            let messageText = "I couldn't run that!";
-            if (!error && response.statusCode == 200) {
-                messageText = body;
-            }
+        // Prepend history of commands
+        var completeCommand = "";
+        const kotBotHist = database.ref('chatroom').child(chatRoomId).child('bots').child('kotbot');
 
-            let newMessage = {
-                isBot: true,
-                text: messageText,
-                isFirstMessageOfDate: false,
-                sendingTime: '2017-09-30T09:47:47.593Z',
-                senderId: botId,
-                senderName: "KotBot"
-            };
-            return chat.sendMessage(this, chatRoomId, newMessage);
-        });
+        kotBotHist.child('imports').orderByChild('seq').once('value').then((imports) => {
+                imports.forEach((importSnapshot) => {
+                    const importNode = importSnapshot.val();
+
+                    if(importNode['command']) {
+                        completeCommand += (lineBreak + importNode['command']);
+                    }
+                });
+            }
+        ).then(kotBotHist.child('commands').orderByChild('seq').once('value').then((commands) => {
+                commands.forEach((commandSnapshot) => {
+                    const commandNode = commandSnapshot.val();
+
+                    if(commandNode['command']) {
+                        completeCommand += (lineBreak + commandNode['command']);
+                    }
+                });
+            }
+        ).then(() => {
+                completeCommand += (lineBreak + command);
+                completeCommand = completeCommand.trim();
+                console.log("Kotlin Executing:" + completeCommand);
+
+                request.post({url: url, headers: headers, body: completeCommand}, function(error, response, body) {
+                    let messageText = "I couldn't run that!";
+                    if(!error && response.statusCode == 200) {
+                        messageText = body;
+
+                        if(command.includes("import")) {
+                            let history = {
+                                seq: new Date().toISOString(),
+                                command: command
+                            };
+
+                            database.ref('chatroom').child(chatRoomId).child('bots').child('kotbot').child('imports').push().set(history);
+                        } else {
+                            let history = {
+                                seq: new Date().toISOString(),
+                                command: command
+                            };
+
+                            database.ref('chatroom').child(chatRoomId).child('bots').child('kotbot').child('commands').push().set(history);
+                        }
+                    }
+
+                    if(messageText === "") { messageText = "done"; }
+                    console.log("Kotlin Result: " + messageText);
+                    let newMessage = {
+                        isBot: true,
+                        text: messageText,
+                        isFirstMessageOfDate: false,
+                        sendingTime: '2017-09-30T09:47:47.593Z',
+                        senderId: botId,
+                        senderName: "KotBot"
+                    };
+                    return chat.sendMessage(this, chatRoomId, newMessage);
+                })
+            }
+        ));
     }
 };
 
