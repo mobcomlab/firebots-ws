233dd99d06c273eda7637cc7fb930b64bc178fd6
Charles Allen <charles.allen@codeconfident.com>
2017-10-01 18:31:42 +0700

    GeoChats - Invite new users if within geofence

--- functions/geochats/invite.js
+++ functions/geochats/invite.js
@@ -1,34 +1,76 @@
 'use strict';
 
 const admin = require('firebase-admin');
+const database = admin.database();
 
 const RANGE = 2;
 
-exports.invite = (event) => {
-    const database = admin.database();
-    const chatroom = event.data.val();
-    const chatroomID = event.params.roomId;
+exports.inviteNewRoom = (event) => {
+    const roomId = event.params.roomId;
+    const room = event.data.val();
 
-    const locationLong = chatroom['long'];
-    const locationLat = chatroom['lat'];
+    const roomLat = room['lat'];
+    const roomLong = room['long'];
+    if(!roomLat || !roomLong) {
+        return; // No location
+    }
 
     return database.ref('user').once('value').then(function(snapshot) {
-        snapshot.forEach(function(childSnapshot) {
-            const childKey = childSnapshot.key;
-            const childData = childSnapshot.val();
+        snapshot.forEach(function(userSnapshot) {
+            const user = userSnapshot.val();
 
-            //console.log(childKey);
-            //console.log(childData);
+            const userLat = user['lat'];
+            const userLong = user['long'];
 
-            if(childData['lat'] && childData['long']) {
-                const distanceFromChat = distanceBetween(locationLat, locationLong, childData['lat'], childData['long']);
+            if(userLat && userLong) {
+                const distanceFromChat = distanceBetween(roomLat, roomLong, userLat, userLong);
                 if(distanceFromChat < RANGE) {
-                    // SEND NOTIFICATION INVITING TO CHAT
-                    sendNotification(childData['token'], chatroomID)
+                    sendNotification(user['token'], roomId)
                 }
             }
         });
     });
+};
+
+exports.inviteNewUser = (event) => {
+    const userOld = event.data._data;
+    const user = event.data.val();
+
+    const userLat = user['lat'];
+    const userLong = user['long'];
+
+    if(!userLat || !userLong) {
+        return; // No location
+    }
+
+    const userLatOld = userOld['lat'];
+    const userLongOld = userOld['long'];
+
+    if(userLatOld && userLongOld && distanceBetween(userLat, userLong, userLatOld, userLongOld) < (RANGE / 2)) {
+        return; // Not moved much
+    }
+
+    return database.ref('chatroom').once('value').then(function(snapshot) {
+        snapshot.forEach(function(roomSnapshot) {
+            const roomId = roomSnapshot.key;
+            const room = roomSnapshot.val();
+
+            const roomLat = room['lat'];
+            const roomLong = room['long'];
+
+            if(roomLat && roomLong) {
+                console.log(roomSnapshot.key);
+                console.log(roomLat);
+                console.log(roomLong);
+
+                const distanceFromChat = distanceBetween(roomLat, roomLong, userLat, userLong);
+                if(distanceFromChat < RANGE) {
+                    sendNotification(user['token'], roomId)
+                }
+            }
+        });
+    });
+
 };
 
 const R = 6371; // Radius of the earth in km
@@ -53,7 +95,7 @@ const distanceBetween = function(lat1, long1, lat2, long2) {
 };
 
 function sendNotification(registrationToken, chatroomID) {
-    var payload = {
+    const payload = {
         notification: {
             title: "You have invitation to chatroom.",
             body: "You want to join this chatroom?",
--- functions/index.js
+++ functions/index.js
@@ -8,7 +8,8 @@ const geochats = require('./geochats/invite');
 const manager = require('./firebots/manager');
 
 // Triggers
-exports.geochatInvite = functions.database.ref('chatroom/{roomId}').onCreate(geochats.invite);
+exports.geochatInviteNewRoom = functions.database.ref('chatroom/{roomId}').onCreate(geochats.inviteNewRoom);
+exports.geochatInviteNewUser = functions.database.ref('user/{userId}').onUpdate(geochats.inviteNewUser);
 
 exports.chatJoined = functions.database.ref('chatroom/{roomId}/user/{userId}').onCreate(manager.chatJoined);
 exports.chatMessage = functions.database.ref('chatroom/{roomId}/message/{pushId}').onCreate(manager.chatMessage);
